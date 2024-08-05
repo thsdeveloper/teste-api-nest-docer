a062dd1af7ddf156e06e0a9143811b8c09e4f247
Thiago Tepê <ths.pereira@gmail.com>
2024-08-05 19:59:31 -0300

    Deploy Google Artefactory v2.1

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000; // Default para 3000 se não estiver definido
+  const port = process.env.PORT || 8080; // Default para 8080 se não estiver definido
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
