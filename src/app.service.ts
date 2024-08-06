f5b3834b94967eaed006e6d606179052a6e5d632
Thiago Tepê <ths.pereira@gmail.com>
2024-08-06 19:12:06 -0300

    Deploy Google Artefactory v3.7

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!. Google Deploy Dockerfile. Sucesso! Run 8080 -> FlashInvesting';
+    return 'Hello World!. Google Deploy Dockerfile. Sucesso! Run 8080 -> FlashInvesting v1.4.87';
   }
 }
